fa438142cd945e73ced8aecb02c92b221e19f922
stevenysy <stevenjxhc@gmail.com>
2023-10-28 21:25:50 -0500

    add play card code

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,14 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import card1 from "./assets/cards/card_1.png";
+import card2 from "./assets/cards/card_2.png";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
 import Tutorial from "./pages/Tutorial";
+import { useState } from "react";
 
 const Main = () => {
+  const [hand, setHand] = useState([1, 2, 1, 2]);
+
   return (
     <Routes>
       <Route exact path="/" Component={Home}></Route>
       <Route exact path="/game" Component={Game}></Route>
-      <Route exact path="/tutorial" Component={Tutorial}></Route>
+      <Route
+        exact
+        path="/tutorial"
+        element={<Tutorial hand={hand} setHand={setHand} />}
+        // Component={Tutorial}
+      ></Route>
     </Routes>
   );
 };
--- frontend/src/pages/Tutorial.js
+++ frontend/src/pages/Tutorial.js
@@ -1,9 +1,32 @@
-import card from "../assets/cards/card_1.png";
 import handLeft from "../assets/hand-left.png";
 import handRight from "../assets/hand-background-right.png";
 import { motion } from "framer-motion";
 
-const Tutorial = () => {
+const Tutorial = ({ hand, setHand }) => {
+  // const reqs = [(num) => num % 2 === 0, (num) => num % 2 !== 0];
+
+  // const checkReqs = function (num) {
+  //   let count = 0;
+  //   reqs.forEach((req, index) => {
+  //     if (req(num)) {
+  //       reqs.splice(index, 1);
+  //       count++;
+  //     }
+  //   });
+
+  //   return count;
+  // };
+
+  /**
+   * Generates a new hand by removing the card that has been played
+   * @param {Number} index The index of the card to be removed
+   */
+  const generateNewHand = function (index) {
+    const newHand = hand.slice();
+    newHand.splice(index, 1);
+    setHand(newHand);
+  };
+
   return (
     <>
       <h1>Tutorial</h1>
@@ -15,22 +38,29 @@ const Tutorial = () => {
           alt="hand right"
         />
         <ul className="hand">
-          <li className="card">
-            <motion.img
-              className="card"
-              src={card}
-              alt="card"
-              whileHover={{ y: -20 }}
-            />
-          </li>
-          <li className="card">
-            <motion.img
-              className="card"
-              src={card}
-              alt="card"
-              whileHover={{ y: -20 }}
-            />
-          </li>
+          {hand.map((cardVal, index) => {
+            return (
+              <li className="card" key={index}>
+                <motion.img
+                  className="card"
+                  src={require(`../assets/cards/card_${cardVal}.png`)}
+                  alt="card"
+                  whileHover={{ y: -20 }}
+                  onClick={() => {
+                    console.log(`played card with value ${cardVal}`);
+
+                    // check if card meets any of the requirements
+                    // const numSatisfied = checkReqs(cardVal);
+                    // console.log(`satisfied ${numSatisfied} requirements`);
+                    // console.log(reqs);
+
+                    // if so, remove the played card from hand
+                    generateNewHand(index);
+                  }}
+                />
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
