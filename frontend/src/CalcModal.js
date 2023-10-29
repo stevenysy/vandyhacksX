83141d9e3206b9c7712ce3a16e60e3d469babef9
stevenysy <stevenjxhc@gmail.com>
2023-10-29 08:23:38 -0500

    add calculation modal

---
+++ frontend/src/CalcModal.js
@@ -0,0 +1,7 @@
+import calcModal from "./assets/calc-modal.png";
+
+const CalcModal = () => {
+  return <img src={calcModal} alt="calculation modal" className="calc-modal" />;
+};
+
+export default CalcModal;
